aeec1cbb1601137857a7d448341649e1135e7e44
Dngn20 <dngn20@gmail.com>
2017-01-23 12:23:11 -0700

    Initial Commit

---
+++ FirstCPlusPlusPorject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstCPlusPlusPorject
+//
+//  Created by Nguyen, Duncan on 1/23/17.
+//  Copyright © 2017 Nguyen, Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
